598c999c2e2fbc17baf9a0973e98315ce8f248b4
AminS128 <aminsmith128@gmail.com>
2025-03-11 14:30:55 -0400

    progress dump

---
+++ display.js
@@ -0,0 +1,88 @@
+
+const ignorelist = [
+    'Record Time',
+    'Record Time Client',
+    'Submit button',
+    'User ID',
+    'algae button',// button values in the json, not needed
+    'coral button',
+    'level 1 butto',
+    'level 2 butto',
+    'level 3 butt',
+    'level 4 butt',
+    'rid',
+    'set up tab',
+    'setup tab',
+    'teleop tab',
+    'state code',// since this is probably known
+    'year',// probably known
+    'comp name',// ..
+    'comp codes',// ..
+    'auto tab',
+    'end tab',
+    'cancel algae',
+    'cancel',
+    'serverCreatedAt',
+    'dropped button'
+]// tags to ignore when displaying data
+
+const listlist = [// list of keys which correspond to fields which are a list of timestamps rather than a counter
+    'level-1',
+    'level-2',
+    'level-3',
+    'level-4',
+    'net',
+    'processor'
+]
+
+
+
+function displayDataR(element, toLoad, n=0){
+    // takes data from data var
+
+    if(toLoad == null || toLoad == undefined){return}
+
+
+    const keys = Object.keys(toLoad)
+    if(keys == [] || keys == undefined || typeof toLoad == 'string' || typeof toLoad == 'number' || typeof toLoad == 'boolean'){
+        
+        element.innerText+=": " + String(toLoad)
+
+    return}
+
+    if(n > 10){return}// shouldnt happen
+
+
+
+
+
+    for(var i = 0; i < keys.length; i ++){
+        if(ignorelist.includes(keys[i])){continue}
+
+        if(toLoad[keys[i]]==null){continue}
+        if(toLoad[keys[i]]==0){continue}
+
+        //hardcoding for specific key values
+        if(listlist.includes(keys[i])){
+            const ndiv = document.createElement('div')
+            ndiv.className = "con" + ((n%2)+1)
+            ndiv.innerText=keys[i] + ": " + (toLoad[keys[i]].length || 1)
+            // ndiv.innerText.replace('undefined', '1')// lmao
+            element.appendChild(ndiv)
+            continue
+        }
+
+
+        const ndiv = document.createElement('div')
+        ndiv.className = "con" + ((n%2)+1)
+        ndiv.innerText=keys[i]
+        element.appendChild(ndiv)
+        displayDataR(ndiv, toLoad[keys[i]], n+1)
+    }
+
+}
+
+function displayData(toLoad=data){
+    document.getElementById('base').innerHTML = ''//clear
+    displayDataR(document.getElementById('base'), toLoad, 0)
+}
---
+++ priority.js
@@ -0,0 +1,104 @@
+var prioritylist = JSON.parse(localStorage.getItem('priorityList') || "[]")
+// contains objects from the processed data (returned from dostuff())
+
+function saveList(list=prioritylist){
+    localStorage.setItem('priorityList', JSON.stringify(list))
+}
+
+function loadList(list=prioritylist){// more like display than load, wtv
+
+    const base = document.getElementById('listbase')
+    base.innerHTML = ""
+    const sample = document.getElementById('sample')
+
+    for(var i = 0; i < list.length; i ++){
+        // console.log(list)
+        const newe = sample.cloneNode(true)
+        newe.removeAttribute('id')
+        newe.className = ""// remove hidden
+        newe.innerHTML = newe.innerHTML
+        .replace("[[T]]", list[i].team)
+        .replace("[[C]]", 
+            (list[i].l4 ? "L4 " : "") +
+            (list[i].l3 ? "L3 " : "") + 
+            (list[i].doesAlgae ? "Algae " : "") +
+            (list[i].auto.coral ? "Auto " : "") + // only counts coral in auto, since moving is p much always and algae doesnt happen
+            (list[i].cage == 2 ? "Deep" : (list[i].cage == 1 ? "Shallow" : ""))
+        )
+        .replace("[[N]]", list[i].notes)
+
+        const moveListener = function(e){// movelistener for elements with a mousedown/mouseup, to be added and removed as needed
+            newe.style.position = 'fixed'
+            // console.log(e)
+            const offset = 10
+            newe.style.left = e.pageX - offset + "px"
+            newe.style.top = e.pageY - offset + "px"
+            // console.log(newe)
+            // console.log(newe)
+        }
+
+        newe.children[0].addEventListener('mousedown', (e)=>{
+            // console.log('on')
+            window.addEventListener('mousemove', moveListener)
+
+        })
+        newe.children[0].addEventListener('mouseup', (e)=>{
+            // console.log('off')
+
+            window.removeEventListener('mousemove', moveListener)
+
+            // reorder
+            var newIndex = 0
+            while (base.children[newIndex].getBoundingClientRect().top + window.scrollY + base.children[newIndex].getBoundingClientRect().height/2 < e.pageY && newIndex < base.children.length-1){
+                newIndex++
+                // console.log(base.children[newIndex].getBoundingClientRect().top + window.scrollY)
+            }
+
+            // console.log(newIndex)
+            var temp = prioritylist.splice(Array.prototype.indexOf.call(base.children, newe), 1)[0]
+            prioritylist.splice(newIndex, 0, temp)
+            
+            saveList()
+            loadList()
+        })
+
+        // console.log(newe.children)
+        // i LOVE hardcoded indeces
+        const a = i
+        newe.children[3].addEventListener('input', ()=>{// tried focusout, definitely more efficient but bugged w drag reordering
+            prioritylist[a].notes = newe.children[3].innerText
+            // console.log(newe.children[3].innerText)
+            saveList()// how it feels to write to localstorage twice a second
+        })
+
+        base.appendChild(newe)
+    }
+}
+
+function addToList(n){
+    var topush = results[n];
+
+    if(!topush){console.log("no data on team");
+        topush = {team:n, auto:{}, notes:"no data"}
+    }// null, undefined, etc (empty object is truthy)
+
+    //item taken from computed results, same format
+    prioritylist.push(topush)
+    saveList()
+    loadList()
+}
+
+function removeFromList(n){
+    for(var i = 0; i < prioritylist.length; i++){
+        if(prioritylist[i].team == n){
+            prioritylist.splice(i, 1)
+            saveList()
+            loadList()
+            return true
+        }
+    }
+    return false
+}
+
+
+loadList()
